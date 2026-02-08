c964837a2e34d3c4813f40c228a7eb0a2d64950c
Team 7 <nirdlaatsoca7@gmail.com>
2026-02-08 13:46:00 +0800

    02_First_App_Acosta

--- 02_First_App_Acosta/src/database/factories/UserFactory.php
+++ 02_First_App_Acosta/src/database/factories/UserFactory.php
@@ -24,7 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->first_name(),
+            'last_name' => fake()->last_name(),
+            'gender' => fake()->randomElement(['Male','Female']),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
