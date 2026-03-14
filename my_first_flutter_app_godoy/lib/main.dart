b90faa1aca17a0a4b70a62ad2ffd6597cbe9e214
Josie <godoyjosie68@gmail.com>
2026-03-14 20:14:04 +0800

    05_My_First_Flutter_App_Godoy

--- my_first_flutter_app_godoy/lib/main.dart
+++ my_first_flutter_app_godoy/lib/main.dart
@@ -72,6 +72,11 @@ void _decrementCounter() {
                 color: Colors.green,
                 ),
               ),
+              const Text ('You have pushed the button this many times:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
           ],
         ),
       ),
