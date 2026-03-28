94c34a41ebde2e931fe78f057484ce56433f7151
Bangonan, Jasmin Joy <bangonanjasminjoy@gmail.com>
2026-03-28 20:20:51 +0800

    06_Adopt_A_Dog_Bangonan

--- adopt_a_dog/lib/main.dart
+++ adopt_a_dog/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:adopt_a_dog/screens/breed_list_screen.dart';
+import 'package:adopt_a_dog/screens/favorites_screen.dart';
+import 'package:adopt_a_dog/services/cache_service.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize cache service
+  await CacheService.instance.init();
+  
   runApp(const AdoptADogApp());
 }
 
@@ -14,21 +21,68 @@ class AdoptADogApp extends StatelessWidget {
       title: 'Adopt-a-Dog',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorSchemeSeed: Colors.green,
+        colorSchemeSeed: Colors.red,
         useMaterial3: true,
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.red,
           foregroundColor: Colors.white,
         ),
         listTileTheme: const ListTileThemeData(
-          iconColor: Colors.greenAccent,
-          textColor: Colors.green,
-          shape: Border(
-            bottom: BorderSide(width: 1)
-          ),
-        )
+          iconColor: Colors.redAccent,
+          textColor: Colors.red,
+          shape: Border(bottom: BorderSide(width: 1)),
+        ),
       ),
-      home: BreedListScreen(),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    BreedListScreen(),
+    FavoritesScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Adopt-a-Dog'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Breeds',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
