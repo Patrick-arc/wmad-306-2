2b9b0a5c8536fddb2959d76df06c2f6a0d5f7210
Joven Paulo Aquisan <aquisanjpQgmail.com>
2026-03-29 13:26:10 +0800

    06_Adopt_A_Dog_Aquisan

--- adopt_a_dog_Aquisan/lib/models/breed.dart
+++ adopt_a_dog_Aquisan/lib/models/breed.dart
@@ -4,5 +4,6 @@ class Breed {
 
   const Breed({required this.name, required this.subBreeds});
 
-  String displayName({String? sub}) => sub != null ? "$sub $name" : name;
+  String displayName([String? sub]) =>
+      sub != null ? '$sub $name' : name;
 }
