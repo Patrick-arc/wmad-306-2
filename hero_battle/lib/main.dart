2123631f952cf8cf03491a1ed6b326a546806d96
Daryll Daniel Cirilo <d412yll@gmail.com>
2026-04-17 08:01:39 +0800

    07_Hero_Battle_App_Cirilo

--- hero_battle/lib/main.dart
+++ hero_battle/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'router/app_router.dart';
 import 'providers/player_provider.dart';
+import 'providers/hero_search_provider.dart';
+import 'providers/deck_provider.dart';
+import 'providers/battle_provider.dart';
 
 void main() => runApp(const HeroBattleApp());
 
@@ -13,6 +16,9 @@ class HeroBattleApp extends StatelessWidget {
     return MultiProvider(
       providers:[
         ChangeNotifierProvider(create: (_) => PlayerProvider()),
+        ChangeNotifierProvider(create: (_) => HeroSearchProvider()),
+        ChangeNotifierProvider(create: (_) => DeckProvider()),
+        ChangeNotifierProvider(create: (_) => BattleProvider()),
       ],
       child: Consumer<PlayerProvider>(
         builder: (context, player, _) {
@@ -29,14 +35,24 @@ class HeroBattleApp extends StatelessWidget {
   }
 
   ThemeData _darkTheme() => ThemeData(
-    colorSchemeSeed: const Color(0xFF7B2FBE),
+    colorSchemeSeed: const Color(0xFFE53935),
     brightness: Brightness.dark,
     useMaterial3: true,
+    scaffoldBackgroundColor: const Color(0xFF121212),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF1E1E1E),
+      foregroundColor: Colors.white,
+    ),
   );
 
   ThemeData _lightTheme() => ThemeData(
-    colorSchemeSeed: const Color(0xFF7B2FBE),
+    colorSchemeSeed: const Color(0xFFE53935),
     brightness: Brightness.light,
     useMaterial3: true,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFFE53935),
+      foregroundColor: Colors.white,
+    ),
   );
 }
